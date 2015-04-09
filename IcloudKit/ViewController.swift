70b589c3c389673f11f3764999cb0303ab46b501
Michel Streblow <michel.streblow@groupon.com>
2015-04-09 17:38:18 +0200

    another changed

--- IcloudKit/ViewController.swift
+++ IcloudKit/ViewController.swift
@@ -7,13 +7,91 @@
 //
 
 import UIKit
+import CloudKit
 
-class ViewController: UIViewController {
+var feedResults = [nameList]()
 
+class nameList {
+    
+    var record: CKRecord
+    var name: String
+    
+    init(record: CKRecord) {
+        
+        self.record = record
+        self.name = record.objectForKey("name") as! String
+        
+        
+    }
+}
+
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+
+    
+
+    @IBOutlet weak var nameText: UITextField!
+    var tableView: UITableView!
+
+
+    var record: CKRecord = CKRecord(recordType: "test")
+   
+    
+    let items = ["Hello 1", "Hello 2", "Hello 3", "Hallo 4"]
+    
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+       
+        let record = feedResults[indexPath.row]
+        cell.textLabel?.text = record.name
+        
+        //cell.textLabel?.text = "\(self.items[indexPath.row])"
+        
+        
+        return cell
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+
+    @IBAction func buttonSave(sender: AnyObject) {
+        
+        // Create record to save tasks
+        
+        // Save task description for key: taskKey
+        record.setObject(self.nameText.text, forKey: "name")
+        
+        // Create the private database for the user to save their data to
+        var database: CKDatabase = CKContainer.defaultContainer().publicCloudDatabase
+        
+        // Save the data to the database for the record: task
+        func recordSaved(record: CKRecord?, error: NSError?) {
+            
+            if (error != nil) {
+                // handle it
+                println(error)
+            }
+        }
+        
+        // Save data to the database for the record: task
+        database.saveRecord(record, completionHandler: recordSaved)
+        println("DONE \(database)")
+        
+    }
+ 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,4 +100,3 @@ class ViewController: UIViewController {
 
 
 }
-
